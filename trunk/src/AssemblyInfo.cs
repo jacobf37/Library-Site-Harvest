c148d68936ca1cbb4570c054b9ceb8b5d88c0503
Robert Scheller <rmscheller@gmail.com>
2017-04-27 10:10:05 -0700

    Update AssemblyInfo.cs

--- trunk/src/AssemblyInfo.cs
+++ trunk/src/AssemblyInfo.cs
@@ -3,7 +3,6 @@
 [assembly: AssemblyTitle("Harvest Library")]
 [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyDescription("Harvest Library for LANDIS-II")]
-[assembly: AssemblyCopyright("2014 University of Notre Dame")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
